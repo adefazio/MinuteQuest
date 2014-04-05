c94d1a6f649896a5746c6b5c4e27b31f43535c15
Aaron Defazio <aaron.defazio@gmail.com>
2014-04-05 16:04:50 +1100

    Assed basic game

---
+++ UnityProject/Assets/Actions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		
+		// The Speed animator parameter is set to the absolute value of the horizontal input.
+		if (h > 0.0f) {
+						rigidbody2D.velocity = new Vector2 (10.0f, 0.0f);
+				} else if (h < 0.0f) {
+						rigidbody2D.velocity = new Vector2 (-10.0f, 0.0f);
+				} else {
+						rigidbody2D.velocity = new Vector2 (0.0f, 0.0f);
+				}
+	}
+}
---
+++ UnityProject/Assets/LockCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockCamera : MonoBehaviour {
+
+	public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = camera.transform.position;
+		camera.transform.position = new Vector3(player.position.x, pos.y, pos.z);
+	}
+}
