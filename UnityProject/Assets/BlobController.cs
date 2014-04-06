02bcef41c18f517baf512d7bdc3ef9c9fd7e9cd3
themonk911 <themonk911@gmail.com>
2014-04-06 17:16:22 +1000

    Blob
    Adding blob

---
+++ UnityProject/Assets/BlobController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlobController : MonoBehaviour {
+
+	public float blobSpeed = .02f;
+	public Vector3 offset;			// The offset at which the Health Bar follows the player.
+	
+	private Transform player;		// Reference to the player.
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		transform.position = player.position + offset;
+	
+	}
+
+
+
+
+}
