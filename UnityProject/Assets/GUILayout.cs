b80cc5e05cee2b1290e6ae2945d11db6a2c5d311
Leftshoe <leftshoe@gmail.com>
2014-04-08 21:24:38 +1000

    Modified GUI and Camera to conform to white board drawing.

--- UnityProject/Assets/GUILayout.cs
+++ UnityProject/Assets/GUILayout.cs
@@ -8,11 +8,39 @@ public class GUILayout : MonoBehaviour {
 	private int selectedWeapon = 0;
 	private int selectedSpell = 0;
 
-	private string[] weaponNames = {"Sword", "Bow"};
+	GUIStyle textFont;
+
+	private string[] weaponNames = {"Saber", "Ray Gun"};
 	private string[] spellNames = {"Fireball", "Disguise"};
+
+	private int halfW = 1920/2;
+	private int halfH = 1080/2;
+	private int swidth = 1920;
+	private int sheight = 1080;
+
+	public void Start() {
+
+	}
+
+	void setupFonts() {
+		textFont = new GUIStyle(GUI.skin.button);
+		textFont.fontSize = 90;
+		textFont.alignment = TextAnchor.MiddleCenter;
+		textFont.normal.textColor = Color.white;
+	}
 
 	void OnGUI ()
 	{
+		setupFonts ();
+
+		// All measurements are in reference to a 1080p screen. 
+		//This resizes the units to be correct for other screen sizes.
+		//Debug.Log ("Screen width " + Screen.width + " height " + Screen.height);
+		Vector2 resizeRatio = new Vector2(((float)Screen.width)/1920.0f, 
+		                                  ((float)Screen.height/1080.0f));
+		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, 
+		                           new Vector3(resizeRatio.x, resizeRatio.y, 1.0f));
+
 		if(texture == null){
 			texture = new Texture2D(1, 1);
 		}
@@ -27,6 +55,7 @@ public class GUILayout : MonoBehaviour {
 
 
 		// Menu box
+		/* Hide for now
 		GUI.BeginGroup(new Rect(10,10,90,70));
 		GUI.Box(new Rect(0,0,90, 58), "");
 
@@ -36,38 +65,45 @@ public class GUILayout : MonoBehaviour {
 		if(GUI.Button(new Rect(5,33,80,20), "Inventory")) {
 			//TODO
 		}
-
 		GUI.EndGroup();
+		*/
+
+
 
 		/////////////////////
 		/// Bars
 		var defaultColor = GUI.color;
-		GUI.BeginGroup(new Rect(Screen.width/2 - 50, Screen.height - 120, 100, 120));
+		GUI.BeginGroup(new Rect(halfW - 50, sheight - 300, 100, 300));
 		GUI.color = Color.red;
-		GUI.Box(new Rect(0,0,40,120), "", style);
+		GUI.Box(new Rect(0,20,40,250), "", style);
 		GUI.color = Color.blue;
-		GUI.Box(new Rect(60,0,40,120), "", style);
+		GUI.Box(new Rect(60,20,40,250), "", style);
 		GUI.color = defaultColor;
 
 		GUI.EndGroup();
 
 		//////////////////
 		/// Select weapon
-		var areaWidth = Screen.width / 3;
-		GUI.BeginGroup(new Rect(20, Screen.height - 100, areaWidth, 80));
-		GUI.Box(new Rect(0, 0, areaWidth, 80), "");
-
-		selectedWeapon = GUI.SelectionGrid(new Rect (0, 0, areaWidth, 80), 
-		                                   selectedWeapon, weaponNames, 2);
-
+		var areaWidth = halfW-100;
+		GUI.BeginGroup(new Rect(20, sheight-300, areaWidth, 300));
+		GUI.Box(new Rect(20, 20, areaWidth-20, 250), "");
+		
+		selectedWeapon = GUI.SelectionGrid(new Rect (20, 20, areaWidth-20, 250), 
+		                                   selectedWeapon, weaponNames, 2, textFont);
 		GUI.EndGroup();
 
 		/////////////////
 		/// Select spell
-		GUI.BeginGroup(new Rect(Screen.width - 20 - areaWidth, Screen.height - 100, areaWidth, 80));
-		GUI.Box(new Rect(0, 0, areaWidth, 80), "");
-		selectedSpell = GUI.SelectionGrid(new Rect (0, 0, areaWidth, 80), 
-		                                  selectedSpell, spellNames, 2);
+
+		GUI.BeginGroup(new Rect(halfW + 60, sheight-300, areaWidth, 300));
+		GUI.Box(new Rect(20, 20, areaWidth-20, 250), "");
+		
+		selectedWeapon = GUI.SelectionGrid(new Rect (20, 20, areaWidth-20, 250), 
+		                                   selectedSpell, spellNames, 2, textFont);
 		GUI.EndGroup();
+
+		///////////////
+		/// Timer
+		GUI.Box(new Rect(halfW-156, 0, 312, 127), "5:00", textFont);
 	}
 }
