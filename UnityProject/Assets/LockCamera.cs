786c2cf45280f69bb667c57446c9dd921e0ea38f
Leftshoe <leftshoe@gmail.com>
2014-04-08 21:24:38 +1000

    Modified GUI and Camera to conform to white board drawing.

--- UnityProject/Assets/LockCamera.cs
+++ UnityProject/Assets/LockCamera.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class LockCamera : MonoBehaviour {
 
 	public Transform player;
-	
+	public enum CameraTypes { Centered, ForwardLooking };
+	public CameraTypes cameraType = CameraTypes.Centered;
+
 	// Update is called once per frame
 	void Update () {
-		var pos = camera.transform.position;
-		camera.transform.position = new Vector3(player.position.x, pos.y, pos.z);
+		switch(cameraType){
+			case CameraTypes.Centered:
+				var pos = camera.transform.position;
+				camera.transform.position = new Vector3(player.position.x, pos.y, pos.z);
+				break;
+		}
 	}
 }
