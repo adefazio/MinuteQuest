cfd02d66808ce937a1ac716dd242ef8cb39401ca
trumpetmagic <trumpet.magic@gmail.com>
2014-04-11 22:47:36 +1000

    Made items

---
+++ UnityProject/Assets/Item.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+/*struct ItemStruct {
+	public string name;
+	public bool prefix;
+	public Item.Stat stat;
+
+
+}*/
+
+public class Item : MonoBehaviour {
+
+	public enum Stat
+		{
+			FIRE, ICE, WATER, RADIATION, ELECTRICITY, PSYCHIC, EARTH, STAGGERING, SMELLY
+		}
+
+	public Stat iStat;
+	public float iValue;
+	public float iLevel = 1.0f;
+	public string iName;
+
+	private System.Random r = new System.Random ();
+
+	// Use this for initialization
+	void Start () {
+		Array stats = Enum.GetValues(typeof(Stat));
+		iStat = (Stat)stats.GetValue (r.Next (stats.Length));
+		iValue = (float)r.NextDouble () * iLevel;
+		iName = iStat.ToString () + " GEM";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
