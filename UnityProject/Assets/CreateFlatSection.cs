dfe461da3001c64ede3afed44fe59f1372ef85e0
Geoff Stacey <geoff_Gs@hotmail.com>
2014-04-11 20:52:47 +1000

    Level spawns indefinitely, player eventually reaches rightmost wall that you can't see

--- UnityProject/Assets/CreateFlatSection.cs
+++ UnityProject/Assets/CreateFlatSection.cs
@@ -9,9 +9,9 @@ public class CreateFlatSection : MonoBehaviour {
 	public Transform t;
 	// Use this for initialization
 	void Start () {
-		Instantiate (floor, t.position + Vector3.down * 2.35f, t.rotation);
-		Instantiate (wall, t.position, t.rotation);
-		Instantiate (roof, t.position + Vector3.up * 2.35f, t.rotation);
+		Instantiate (flatFloor, t.position + Vector3.down * 2.35f, t.rotation);
+		Instantiate (flatWall, t.position, t.rotation);
+		Instantiate (flatRoof, t.position + Vector3.up * 2.35f, t.rotation);
 	}
 	
 	// Update is called once per frame
--- UnityProject/Assets/CreateLevel.cs
+++ UnityProject/Assets/CreateLevel.cs
@@ -9,21 +9,33 @@ public class CreateLevel : MonoBehaviour {
 	public GameObject leftWall;
 	public GameObject rightWall;
 	public Transform t;
+	private Transform player;
+	private float tileWidth;
+	private float tileHeight;
 
 	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		SpriteRenderer s = flatWall.GetComponent<SpriteRenderer> ();
+		tileWidth = s.bounds.size.x;
+		tileHeight = s.bounds.size.y;
 		t.position = new Vector3 (0f, 0f, 0f);
-		Instantiate (leftWall, t.position + Vector3.left * 3.75f, t.rotation);
-		for (int i = 0; i < 10; i++) {
-			Instantiate (flatFloor, t.position + Vector3.down * 2.35f, t.rotation);
+		Instantiate (leftWall, t.position + Vector3.left * tileWidth * 0.5f, t.rotation);
+		for (int i = 0; i < 5; i++) {
+			Instantiate (flatFloor, t.position + Vector3.down * tileHeight * 0.5f, t.rotation);
 			Instantiate (flatWall, t.position, t.rotation);
-			Instantiate (flatRoof, t.position + Vector3.up * 2.35f, t.rotation);
-			t.position = t.position + Vector3.right * 7.5f;
+			Instantiate (flatRoof, t.position + Vector3.up * tileHeight * 0.5f, t.rotation);
+			t.position = t.position + Vector3.right * tileWidth;
 		}
-		Instantiate (rightWall, t.position + Vector3.left * 3.75f, t.rotation);
+		Instantiate (rightWall, t.position + Vector3.right * tileWidth * 50.5f, t.rotation);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (player.position.x > t.position.x - tileWidth * 1.5f) {
+			Instantiate (flatFloor, t.position + Vector3.down * tileHeight * 0.5f, t.rotation);
+			Instantiate (flatWall, t.position, t.rotation);
+			Instantiate (flatRoof, t.position + Vector3.up * tileHeight * 0.5f, t.rotation);
+			t.position = t.position + Vector3.right * tileWidth;
+		}
 	}
 }
