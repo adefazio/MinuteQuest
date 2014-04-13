a8c53e81f5c711c0d6b77dda3aca3ac75a77c085
Leftshoe <leftshoe@gmail.com>
2014-04-13 17:32:24 +1000

    Moved a lot of things around, and added new blob sprite

---
+++ UnityProject/Assets/ParticleEffects/FireballScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballScript : MonoBehaviour {
+
+	private float generateParticlesFor = 1.0f;
+	private float killAfter = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		// Damage everything under fireball
+		//TODO
+
+		Invoke ("turnOffEffect", generateParticlesFor);
+		Invoke ("kill", killAfter);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void turnOffEffect() {
+		foreach (Transform child in transform) {
+			var epe = child.GetComponent<ParticleEmitter>();
+			epe.emit = false;
+		}
+
+	}
+
+	void kill() {
+		Destroy(gameObject);
+	}
+}
