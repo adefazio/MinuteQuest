52c64aa1894942194f16e042e9f0b0b7e8efbf26
trumpetmagic <trumpet.magic@gmail.com>
2014-04-15 16:39:37 +1000

    Added parallax scrolling script

---
+++ UnityProject/Assets/MoveLayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLayer : MonoBehaviour {
+
+	//speed at which the layer moves
+	//speed 1 means move with camera
+	//speed 0 means stay fixed in scene
+	public float speed = 0.5f;
+	private Camera mainCamera;
+	private Vector3 cameraPosition;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindObjectOfType<Camera>();
+		cameraPosition = mainCamera.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += (mainCamera.transform.position - cameraPosition) * speed;
+		cameraPosition = mainCamera.transform.position;
+	}
+}
